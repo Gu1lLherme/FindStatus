fb2a8f08dd5e6e664c510d942734a1b7b2131991
Guilherme Eugênio <guilhermebarreto.aju@gmail.com>
2025-02-17 10:07:01 -0300

    Estrutura

---
+++ src/main.py
@@ -0,0 +1,9 @@
+import pandas as pd
+
+
+
+tabela_referencia = pd.read_csv('C:\Users\gesbarreto\Downloads\FindStatus\src\Tabelas\Suplementos Aracaju com Endereço.csv', sep=';', )
+
+print
+tabela_consulta = pd.read_csv('.csv')
+
