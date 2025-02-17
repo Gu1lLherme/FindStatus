eee81473a0847dfdc8e202fe023223cb12b1873d
Guilherme Eugenio <guilhermebarreto.aju@gmail.com>
2025-02-17 11:46:33 -0300

    "UPDATE: Adicionando arquivos de resultado Implementação de extração do LOGRADOURO e Número"

--- src/main.py
+++ src/main.py
@@ -1,9 +1,19 @@
 import pandas as pd
+import tratamentoDados as td
 
+# Carregar tabelas de referência e consulta
+tabela_referencia = pd.read_csv('C:\\Users\\gesbarreto\\Downloads\\FindStatus\\src\\Tabelas\\Suplementos Aracaju com Endereço.csv', sep=';', on_bad_lines='skip')
+tabela_consulta = pd.read_csv('C:\\Users\\gesbarreto\\Downloads\\FindStatus\\src\\Tabelas\\estabelecimentos_Veiculos em Aracaju, SE.csv', sep=';', on_bad_lines='skip')
 
+# Aplicar a função em cada linha do DataFrame
+tabela_referencia[["LOGRADOURO-TEXTO", "NUMERO"]] = tabela_referencia["LOGRADOURO"].apply(
+    lambda x: pd.Series(td.extrair_endereco(x))
+)
 
-tabela_referencia = pd.read_csv('C:\Users\gesbarreto\Downloads\FindStatus\src\Tabelas\Suplementos Aracaju com Endereço.csv', sep=';', )
+# Exibir colunas das tabelas
+print(tabela_referencia.columns)
+print(tabela_consulta.columns)
 
-print
-tabela_consulta = pd.read_csv('.csv')
+# Salvar dados processados
+td.save_data(tabela_referencia, 'Suplementos Aracaju com Endereço')
 
